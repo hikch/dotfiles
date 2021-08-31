631dcbf52c74ca23cc78d4fa5bb1b0b5db4ed9f9
Heikichi Umehara <heikichi3@gmail.com>
2021-09-01 00:41:29 +0900

    ENV: Change a order in which environment variables are set

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,20 +1,26 @@
 ## 
+# setup shell
+set -x SHELL /opt/homebrew/bin/fish
+
 # setup default editor
 set -x EDITOR vi
+
+# setup homebrew
+eval (/opt/homebrew/bin/brew shellenv)
+
+# openssl
+fish_add_path /opt/homebrew/opt/openssl@1.1/bin
 
 # setup direnv
 eval (direnv hook fish)
 
 # setup pyenv
-status --is-interactive; and source (pyenv init -|psub)
+status is-interactive; and pyenv init --path | source
 pyenv init - | source
 
 # setup gcloud
 # The next line updates PATH for the Google Cloud SDK.
 source "/opt/homebrew/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/path.fish.inc"
-
-# setup homebrew
-eval (/opt/homebrew/bin/brew shellenv)
 
 ## MySQL
 #
@@ -26,6 +32,3 @@ set -x PATH ~/.poetry/bin $PATH
 
 # pip cache
 set -x PIP_DOWNLOAD_CACHE ~/.cache/pip
-
-# openssl
-fish_add_path /opt/homebrew/opt/openssl@1.1/bin
